610d5fe3b71df973267253956f17ab22f7d3afed
Kevin Sitek <sitek.kevin@gmail.com>
2025-11-17 17:09:39 -0600

    Updating code to work with Tian S2 atlas

--- applytransforms_MNI2009cAsym-to-MNINLin6Asym.sh
+++ applytransforms_MNI2009cAsym-to-MNINLin6Asym.sh
@@ -1,8 +1,12 @@
 #!/bin/zsh
 
+tian_scale='S2'
+
+atlas_base=atlas-custom_subcort-tian${tian_scale}_cort-aud-vis-carpet
+
 xfm_fpath='/Users/dsj3886/data_local/reference/tpl-MNI152NLin2009cAsym_from-MNI152NLin6Asym_mode-image_xfm.h5'
 fixed_fpath='/Users/dsj3886/data_local/HCP_3T_structural/100610/MNINonLinear/T1w.nii.gz'
-moving_fpath='/Users/dsj3886/data_local/derivatives/atlas-custom_subcort-tians3_cort-carpet/atlas-custom_subcort-tians3_cort-carpet_atlas.nii.gz'
-out_fpath='/Users/dsj3886/data_local/derivatives/atlas-custom_subcort-tians3_cort-carpet/atlas-custom_subcort-tians3_cort-carpet_atlas_space-MNI152NLin6Asym.nii.gz'
+moving_fpath="/Users/dsj3886/data_local/derivatives/${atlas_base}/${atlas_base}_atlas.nii.gz"
+out_fpath="/Users/dsj3886/data_local/derivatives/${atlas_base}/${atlas_base}_atlas_space-MNI152NLin6Asym.nii.gz"
 
 antsApplyTransforms -i $moving_fpath -r $fixed_fpath -o $out_fpath -n GenericLabel -t ${xfm_fpath} -v
--- msmt_connectome.sh
+++ msmt_connectome.sh
@@ -2,6 +2,8 @@
 
 sub_id=$1
 
+tian_scale='S2'
+
 alg=iFOD2
 nsl=10mil
 data_dir=/Users/dsj3886/data_local/derivatives/HCP_7T_diffusion/msmt_csd_nthreads-1/
@@ -12,14 +14,14 @@ echo "streamline file: ${tck_fpath}"
 
 sift2_weights_fpath=${tck_dir}/sift2_weights_streamlines_alg-${alg}_nsl-${nsl}
 
-desc_base=atlas-custom_subcort-tians3_cort-carpet
+desc_base=atlas-custom_subcort-tian${tian_scale}_cort-aud-vis-carpet
 opt_desc="_sift2"
 out_dir=${tck_dir}/connectome_${sl_base}/${desc_base}${opt_desc}/
 mkdir -p $out_dir
 echo "output directory: ${out_dir}"
 
 atlas_dir=/Users/dsj3886/data_local/derivatives/HCP_7T_diffusion/atlas_space-sub/${sub_id}/
-atlas_fpath=${atlas_dir}/sub-${sub_id}_atlas-custom_subcort-tians3_cort-carpet_atlas_warp-standard2acpc_dc_space-T1w.nii.gz
+atlas_fpath=${atlas_dir}/sub-${sub_id}_${desc_base}_atlas_warp-standard2acpc_dc_space-T1w.nii.gz
 
 echo "generating connectome"
 tck2connectome $tck_fpath \
