6d95ece5c12ab9fee87a61efbd8cf172dbb32e5d
Kevin Sitek <sitek.kevin@gmail.com>
2026-04-17 17:11:28 -0500

    Added -scale_invnodevol to tck2connectome. TODO: remove from conn plots

--- msmt_connectome.sh
+++ msmt_connectome.sh
@@ -14,7 +14,7 @@ echo "streamline file: ${tck_fpath}"
 
 sift2_weights_fpath=${tck_dir}/sift2_weights_streamlines_alg-${alg}_nsl-${nsl}
 
-desc_base=atlas-custom_subcort-tian${tian_scale}_cort-aud-vis-carpet
+desc_base=atlas-custom_subcort-tian${tian_scale}_cort-carpet
 opt_desc="_sift2"
 out_dir=${tck_dir}/connectome_${sl_base}/${desc_base}${opt_desc}/
 mkdir -p $out_dir
@@ -30,6 +30,7 @@ tck2connectome $tck_fpath \
   -tck_weights_in $sift2_weights_fpath \
   -out_assignments ${out_dir}/${sl_base}_assignments${opt_desc}.txt \
   -symmetric \
+  -scale_invnodevol \
   -force
 
 echo "generating edge streamlines"
