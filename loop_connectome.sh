5d1d70b5aa14e7419e0c6eb88364b384c519e102
Kevin Sitek <sitek.kevin@gmail.com>
2026-05-15 15:06:13 -0500

    Added parallel limit in loop_connectome to limit CPU usage

--- loop_connectome.sh
+++ loop_connectome.sh
@@ -1,9 +1,15 @@
 #!/bin/zsh
 
 raw_dir=/Users/dsj3886/data_local/HCP_7T_diffusion/
-for subpath in $raw_dir/2*; do
-  sub_id=$(basename $subpath)
-  #echo $sub_id &
-  zsh msmt_connectome.sh $sub_id &
+max_jobs=16  # tune for M2 Ultra
+
+for subpath in $raw_dir/*; do
+    sub_id=$(basename $subpath)
+    zsh msmt_connectome.sh $sub_id &
+
+    # throttle: wait when max_jobs are running
+    while [[ $(jobs -r | wc -l) -ge $max_jobs ]]; do
+        sleep 2
+    done
 done
 wait
